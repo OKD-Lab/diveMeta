94bb0c2d32f2eef576f1e448e308c05927b95abd
Tadahisa Okuda <tadahisa.okuda@gmail.com>
2025-08-16 10:31:18 +0900

    Rename example CSV to 'meling_grs.csv'; update read_example() and references

--- R/read_example.R
+++ R/read_example.R
@@ -1,10 +1,10 @@
 #' Read example datasets shipped with the package
 #'
 #' @param name character, file name in inst/extdata
-#'   e.g. "oyelade_sdnn.csv" or "meling_grs_medians.csv"
+#'   e.g. "oyelade_sdnn.csv" or "meling_grs.csv"
 #' @return data.frame
 #' @export
-read_example <- function(name = c("oyelade_sdnn.csv", "meling_grs_medians.csv")) {
+read_example <- function(name = c("oyelade_sdnn.csv", "meling_grs.csv")) {
   name <- match.arg(name)
   f <- system.file("extdata", name, package = "diveMeta", mustWork = TRUE)
   utils::read.csv(f, stringsAsFactors = FALSE)
